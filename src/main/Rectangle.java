f27a4e7c02c9710739982a578ed8a28050c873f2
Ole Wellnitz <OleWellnitz@web.de>
2017-12-06 00:57:06 +0100

    Added rectangle object #0001

---
+++ src/main/Rectangle.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Rectangle {
+	private Double sideA = 0.0;
+	private Double sideB = 0.0;
+	
+	/**
+	 * The constructor creates a rectangle.
+	 * 
+	 * @param a Length of side a.
+	 * @param b Length of side b.
+	 * @param h The height of the rectangle.
+	 */
+	public Rectangle (Double a, Double b) {
+		sideA = a;
+		sideB = b;
+	}
+}
